1d5c7ce3bf73e3b446ad82bf98bc567b3fff0dbc
Loiane Groner <loianeg@gmail.com>
2010-06-17 15:58:55 -0300

    ruby strings

---
+++ src/p003rubystrings.rb
@@ -0,0 +1,25 @@
+# p003rubystrings.rb
+# Loiane Groner
+# http://loianegroner.com (English)
+# http://loiane.com (Portuguese)
+
+# p003rubystrings.rb
+=begin
+  Ruby Strings
+  In Ruby, strings are mutable
+=end
+
+puts "Hello World"
+# Can use " or ' for Strings, but ' is more efficient
+puts 'Hello World'
+# String concatenation
+puts 'I like' + ' Ruby'
+# Escape sequence
+puts 'It\'s my Ruby'
+# New here, displays the string three times
+puts 'Hello' * 3
+# Defining a constant
+# More on Constants later, here
+# http://rubylearning.com/satishtalim/ruby_names.html
+PI = 3.1416
+puts PI
