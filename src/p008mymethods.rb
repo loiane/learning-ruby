b3253715b799dd5cdcb322c78c23e3f668ae76ad
Loiane Groner <loianeg@gmail.com>
2010-06-17 16:42:44 -0300

    methods example

---
+++ src/p008mymethods.rb
@@ -0,0 +1,28 @@
+# p008mymethods.rb
+# Loiane Groner
+# http://loianegroner.com (English)
+# http://loiane.com (Portuguese)
+
+# A method returns the value of the last statement
+# Methods that act as queries are often named with a trailing ?
+# Methods that are "dangerous," or modify the receiver, might be named with a trailing ! (Bang methods)
+# A simple method
+def hello
+  puts 'Hello'
+end
+#use the method
+hello
+
+# Method with an argument - 1
+def hello1(name)
+  puts 'Hello ' + name
+  return 'success'
+end
+puts(hello1('Loiane'))
+
+# Method with an argument - 2
+def hello2 name2
+  puts 'Hello ' + name2
+  return 'success'
+end
+puts(hello2 'Groner')
